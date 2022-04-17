8fadb6ba608b9de2e7ec7fc18417aaf2c2ec18c7
Nakib Hossain <hussain0296@gmail.com>
2022-04-17 15:31:01 -0400

    Change Base URL

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
-      ? 'http://ec2-44-241-29-70.us-west-2.compute.amazonaws.com:3001/api/v1' // development api
-      : 'http://ec2-44-241-29-70.us-west-2.compute.amazonaws.com:3001/api/v1' // production api
+      ? 'https://admin.nakib.xyz/api/v1' // development api
+      : 'https://admin.nakib.xyz/api/v1' // production api
       // ? 'http://127.0.0.1:3001/api/v1' // development api
       // : 'http://127.0.0.1:3001/api/v1'
   }
