5dcf5f2dba674d57f8a190f926a4b65f6aa6387e
Nakib Hossain <nakib.hossain@lexisnexisrisk.com>
2022-06-12 14:15:54 -0400

    next config fix

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
-      // ? 'https://admin.nakib.xyz/api/v1' // development api
-      // : 'https://admin.nakib.xyz/api/v1' // production api
-      ? 'http://127.0.0.1:3001/api/v1' // development api
-      : 'http://127.0.0.1:3001/api/v1'
+      ? 'https://admin.nakib.xyz/api/v1' // development api
+      : 'https://admin.nakib.xyz/api/v1' // production api
+    // ? 'http://127.0.0.1:3001/api/v1' // development api
+    // : 'http://127.0.0.1:3001/api/v1'
   }
 }
 
